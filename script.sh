467be959e5302af90dece3157f28d1fe6617b9d6
Vagrant Default User <vagrant@gitbook>
2019-01-30 09:55:54 +0000

    update script

--- script.sh
+++ script.sh
@@ -2,9 +2,10 @@ git commit -am "Actualización"
 git push origin master
 
 gitbook build . prueba
-git checkout gh-pages prueba/*
+git checkout gh-pages prueba
 
 git checkout gh-pages
+git mv prueba/* .
 git add *
 git commit -am "Actualización web"
 git push origin gh-pages
